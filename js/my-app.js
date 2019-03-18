9b8cda39d8a4fe1c61c090d09b97988d08dad5d1
mrpons97 <45408067+mrpons97@users.noreply.github.com>
2019-03-17 21:04:36 -0600

    Add files via upload

--- js/my-app.js
+++ js/my-app.js
@@ -62,69 +62,7 @@ $$(window).on('load', function () {
   notificationWelcome.open();
 });
 
-var app = new Framework7({
-  routes: [{
-    path: '/login-screen/',
-    /*
-    We can load it from url like:
-    url: 'login-screen.html'
-    But in this example we load it just from content string
-    */
-    content: '\
-      <div class="page no-navbar no-toolbar no-swipeback">\
-        <div class="page-content login-screen-content">\
-          <div class="login-screen-title">My App</div>\
-          <form>\
-            <div class="list">\
-              <ul>
-              <li class="item-content item-input">
-                <div class="item-inner">
-                  <div class="item-title item-label">Username</div>
-                  <div class="item-input-wrap">
-                    <input type="text" name="username" placeholder="Username">
-                    <span class="input-clear-button"></span>
-                  </div>
-                </div>
-              </li>
-              <li class="item-content item-input">
-                <div class="item-inner">
-                  <div class="item-title item-label">Password</div>
-                  <div class="item-input-wrap">
-                    <input type="password" name="password" placeholder="Password">
-                    <span class="input-clear-button"></span>
-                  </div>
-                </div>
-              </li>
-            </ul>
-            </div>\
-            <div class="list">\
-              <ul>\
-                <li><a href="#" class="item-link list-button">Sign In</a></li>\
-              </ul>\
-              <div class="block-footer">\
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>\
-                <p><a href="/index/" class="link back">Close Login Screen</a></p>\
-              </div>\
-            </div>\
-          </form>\
-        </div>\
-      </div>'
-    }]
-});
 
-// Login Screen-Modal DOM events
-$$('.login-screen').on('loginscreen:open', function (e, loginScreen) {
-  console.log('Login screen open')
-});
-$$('.login-screen').on('loginscreen:opened', function (e, loginScreen) {
-  console.log('Login screen opened')
-});
-$$('.login-screen').on('loginscreen:close', function (e, loginScreen) {
-  console.log('Login screen close')
-});
-$$('.login-screen').on('loginscreen:closed', function (e, loginScreen) {
-  console.log('Login screen closed')
-});
 
 
 
